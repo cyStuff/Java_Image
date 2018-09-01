982f6f022c04fc4929c3ebbec66f4114c0f72314
cyStuff <cypressr2581@gmail.com>
2018-08-31 19:14:20 -0600

    Add files via upload

---
+++ src/Color.java
@@ -0,0 +1,217 @@
+package src;
+
+import java.util.function.UnaryOperator;
+
+/**
+ * Color class to hold the RGB values of a color.
+ * 
+ * @author cy
+ *
+ */
+public class Color {
+	private int[] dat;
+
+	/**
+	 * Sets the color based on RGB int values.
+	 * 
+	 * @param red
+	 *            - int
+	 * @param green
+	 *            - int
+	 * @param blue
+	 *            - int
+	 */
+	public Color(int red, int green, int blue) {
+		dat = new int[] { red, green, blue };
+	}
+
+	/**
+	 * Sets the color based on an int[].
+	 * 
+	 * @param color
+	 *            - int[3]
+	 */
+	public Color(int[] color) {
+		if (!(color.length == 3)) {
+			throw new ColorException(
+					"Color array must contatain exactly 3 elements.");
+		}
+		dat = color;
+	}
+
+	/**
+	 * Sets a color based on a UnaryOperator.
+	 * 
+	 * @param operator
+	 *            - UnaryOperator applied to the color
+	 * @return Color
+	 */
+	public Color setColor(UnaryOperator<Color> operator) {
+		int[] data = operator.apply(this).getArray();
+		return new Color(data);
+	}
+
+	/**
+	 * Sets all color channels with the same operator.
+	 * 
+	 * @param operator
+	 *            - UnaryOperator applied to all channels seperately
+	 * @return Color
+	 */
+	public Color setAllColor(UnaryOperator<Integer> operator) {
+		dat[0] = operator.apply(dat[0]);
+		dat[1] = operator.apply(dat[1]);
+		dat[2] = operator.apply(dat[2]);
+		return this;
+	}
+
+	/**
+	 * Sets the red color from an integer value.
+	 * 
+	 * @param color
+	 *            - int to define red channel
+	 * @return Color
+	 */
+	public Color setRed(int color) {
+		dat[0] = color;
+		return this;
+	}
+
+	/**
+	 * Sets the red value based on a UnaryOperator.
+	 * 
+	 * @param operator
+	 *            - UnaryOperator to set the red channel
+	 * @return Color
+	 */
+	public Color setRed(UnaryOperator<Integer> operator) {
+		dat[0] = operator.apply(dat[0]);
+		return this;
+	}
+
+	/**
+	 * Sets the green color from an integer value.
+	 * 
+	 * @param color
+	 *            - int to define green channel
+	 * @return Color
+	 */
+	public Color setGreen(int color) {
+		dat[1] = color;
+		return this;
+	}
+
+	/**
+	 * Sets the green value based on a UnaryOperator.
+	 * 
+	 * @param operator
+	 *            - UnaryOperator to set the green channel
+	 * @return Color
+	 */
+	public Color setGreen(UnaryOperator<Integer> operator) {
+		dat[1] = operator.apply(dat[1]);
+		return this;
+	}
+
+	/**
+	 * Sets the blue color from an integer value.
+	 * 
+	 * @param color
+	 *            - int to define blue channel
+	 * @return Color
+	 */
+	public Color setBlue(int color) {
+		dat[2] = color;
+		return this;
+	}
+
+	/**
+	 * Sets the blue value based on a UnaryOperator.
+	 * 
+	 * @param operator
+	 *            - UnaryOperator to set the blue channel
+	 * @return Color
+	 */
+	public Color setBlue(UnaryOperator<Integer> operator) {
+		dat[2] = operator.apply(dat[2]);
+		return this;
+	}
+
+	/**
+	 * Returns the red color value.
+	 * 
+	 * @return the int value for red
+	 */
+	public int getRed() {
+		return dat[0];
+	}
+
+	/**
+	 * Returns the green color value.
+	 * 
+	 * @return the int value for green
+	 */
+	public int getGreen() {
+		return dat[1];
+	}
+
+	/**
+	 * Returns the blue color value.
+	 * 
+	 * @return the int value for blue
+	 */
+	public int getBlue() {
+		return dat[2];
+	}
+
+	/**
+	 * Get the color values as an int[].
+	 * 
+	 * @return int array with 3 values, one for each channel
+	 */
+	public int[] getArray() {
+		return dat;
+	}
+
+	/**
+	 * Set the color based on an int[].
+	 * 
+	 * @param color
+	 *            - int array that must have exactly 3 values
+	 */
+	public void setArray(int[] color) {
+		if (!(color.length == 3)) {
+			throw new ColorException(
+					"Color array must contatain exactly 3 elements.");
+		}
+		dat = color;
+	}
+
+	/**
+	 * Tests if two colors are equivalent.
+	 * 
+	 * @param color
+	 *            - Color being tested.
+	 * @return boolean telling of equivalent
+	 */
+	public boolean equals(Color color) {
+		if (this.getRed() == color.getRed()
+				&& this.getGreen() == color.getGreen()
+				&& this.getBlue() == color.getBlue()) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Exception for colors.
+	 * 
+	 * @author cy
+	 */
+	@SuppressWarnings("serial")
+	public class ColorException extends RuntimeException {
+		public ColorException(String e) {
+			super(e);
+		}
+	}
+}
---
+++ src/Gif.java
@@ -0,0 +1,213 @@
+package src;
+
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.imageio.IIOException;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageOutputStream;
+/**
+ * Static class for creating gifs.
+ * 
+ * @author cy
+ *
+ */
+public final class Gif {
+
+	/**
+	 * Private constructor to prevent instantiation.
+	 */
+	private Gif() {
+	}
+
+	/**
+	 * Makes a gif from an array of Images.
+	 * 
+	 * @param images
+	 *            - Array of Images to be compressed to gif.
+	 * @param frameTime
+	 *            - Time in milliseconds between frames.
+	 * @param fileName
+	 *            - The name of the output file.
+	 */
+	public static void makeGif(BaseImage[] images, int frameTime,
+			String fileName) {
+		try {
+			ImageOutputStream output = new FileImageOutputStream(new File(
+					fileName));
+			GifWriter g = new GifWriter(output, images[0].getBI().getType(),
+					frameTime, true);
+			for (int i = 0; i < images.length; i++) {
+				g.writeToSequence(images[i].getBI());
+			}
+			g.close();
+			output.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Can Not Make Gif.");
+		}
+	}
+
+	/**
+	 * Makes a gif from a List of Images.
+	 * 
+	 * @param images
+	 *            - List of Images to be compressed to gif.
+	 * @param frameTime
+	 *            - Time in milliseconds between frames.
+	 * @param fileName
+	 *            - The name of the output file.
+	 */
+	public static void makeGif(List<BaseImage> images, int frameTime,
+			String fileName) {
+		makeGif(images.toArray(new Image[0]), frameTime, fileName);
+	}
+
+	/**
+	 * Inner class that handles all of the gif stuff.
+	 * 
+	 * @author Created by Elliot Kroo on 2009-04-25.
+	 *
+	 */
+	private static class GifWriter {
+		protected ImageWriter gifWriter;
+		protected ImageWriteParam imageWriteParam;
+		protected IIOMetadata imageMetaData;
+
+		/**
+		 * Creates a new GifSequenceWriter
+		 * 
+		 * @param outputStream
+		 *            the ImageOutputStream to be written to
+		 * @param imageType
+		 *            one of the imageTypes specified in BufferedImage
+		 * @param timeBetweenFramesMS
+		 *            the time between frames in miliseconds
+		 * @param loopContinuously
+		 *            wether the gif should loop repeatedly
+		 * @throws IIOException
+		 *             if no gif ImageWriters are found
+		 *
+		 * @author Elliot Kroo (elliot[at]kroo[dot]net)
+		 */
+		public GifWriter(ImageOutputStream outputStream, int imageType,
+				int timeBetweenFramesMS, boolean loopContinuously)
+				throws IIOException, IOException {
+			// my method to create a writer
+			gifWriter = getWriter();
+			imageWriteParam = gifWriter.getDefaultWriteParam();
+			ImageTypeSpecifier imageTypeSpecifier = ImageTypeSpecifier
+					.createFromBufferedImageType(imageType);
+
+			imageMetaData = gifWriter.getDefaultImageMetadata(
+					imageTypeSpecifier, imageWriteParam);
+
+			String metaFormatName = imageMetaData.getNativeMetadataFormatName();
+
+			IIOMetadataNode root = (IIOMetadataNode) imageMetaData
+					.getAsTree(metaFormatName);
+
+			IIOMetadataNode graphicsControlExtensionNode = getNode(root,
+					"GraphicControlExtension");
+
+			graphicsControlExtensionNode.setAttribute("disposalMethod", "none");
+			graphicsControlExtensionNode.setAttribute("userInputFlag", "FALSE");
+			graphicsControlExtensionNode.setAttribute("transparentColorFlag",
+					"FALSE");
+			graphicsControlExtensionNode.setAttribute("delayTime",
+					Integer.toString(timeBetweenFramesMS / 10));
+			graphicsControlExtensionNode.setAttribute("transparentColorIndex",
+					"0");
+
+			IIOMetadataNode commentsNode = getNode(root, "CommentExtensions");
+			commentsNode.setAttribute("CommentExtension", "Created by MAH");
+
+			IIOMetadataNode appEntensionsNode = getNode(root,
+					"ApplicationExtensions");
+
+			IIOMetadataNode child = new IIOMetadataNode("ApplicationExtension");
+
+			child.setAttribute("applicationID", "NETSCAPE");
+			child.setAttribute("authenticationCode", "2.0");
+
+			int loop = loopContinuously ? 0 : 1;
+
+			child.setUserObject(new byte[] { 0x1, (byte) (loop & 0xFF),
+					(byte) ((loop >> 8) & 0xFF) });
+			appEntensionsNode.appendChild(child);
+
+			imageMetaData.setFromTree(metaFormatName, root);
+
+			gifWriter.setOutput(outputStream);
+
+			gifWriter.prepareWriteSequence(null);
+		}
+
+		public void writeToSequence(RenderedImage img) throws IOException {
+			gifWriter.writeToSequence(new IIOImage(img, null, imageMetaData),
+					imageWriteParam);
+		}
+
+		/**
+		 * Close this GifSequenceWriter object. This does not close the
+		 * underlying stream, just finishes off the GIF.
+		 */
+		public void close() throws IOException {
+			gifWriter.endWriteSequence();
+		}
+
+		/**
+		 * Returns the first available GIF ImageWriter using
+		 * ImageIO.getImageWritersBySuffix("gif").
+		 * 
+		 * @return a GIF ImageWriter object
+		 * @throws IIOException
+		 *             if no GIF image writers are returned
+		 */
+		private ImageWriter getWriter() throws IIOException {
+			Iterator<ImageWriter> iter = ImageIO.getImageWritersBySuffix("gif");
+			if (!iter.hasNext()) {
+				throw new IIOException("No GIF Image Writers Exist");
+			} else {
+				return iter.next();
+			}
+		}
+
+		/**
+		 * Returns an existing child node, or creates and returns a new child
+		 * node (if the requested node does not exist).
+		 * 
+		 * @param rootNode
+		 *            the <tt>IIOMetadataNode</tt> to search for the child node.
+		 * @param nodeName
+		 *            the name of the child node.
+		 * 
+		 * @return the child node, if found or a new node created with the given
+		 *         name.
+		 */
+		private IIOMetadataNode getNode(IIOMetadataNode rootNode,
+				String nodeName) {
+			int nNodes = rootNode.getLength();
+			for (int i = 0; i < nNodes; i++) {
+				if (rootNode.item(i).getNodeName()
+						.compareToIgnoreCase(nodeName) == 0) {
+					return ((IIOMetadataNode) rootNode.item(i));
+				}
+			}
+			IIOMetadataNode node = new IIOMetadataNode(nodeName);
+			rootNode.appendChild(node);
+			return (node);
+		}
+
+	}
+
+}
