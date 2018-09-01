23af359132fa656fb1afbb3ff8d667bf1c25e43e
cyStuff <cypressr2581@gmail.com>
2018-08-31 21:22:15 -0600

    Add files via upload

--- src/package-info.java
+++ src/package-info.java
@@ -1,7 +1,7 @@
 /**
 *Provides classes for editing images
 *<br><br>
-*<a href="https://github.com/cyStuff/Java_Image">GitHub Page</a>
+*<a href="https://github.com/cyStuff/Java_Image" target="_blank">GitHub Page</a>
 *<br>
 */
 package src;
